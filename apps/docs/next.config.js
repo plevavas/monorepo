68e7f94923f8e9378bdae24a47f126ead73e7c06
Pierre Levavasseur <pierre@scorenco.com>
2022-10-07 11:38:09 +0200

    add standalone config

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,5 +1,13 @@
+const path = require("path");
+
 const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../.."),
+  },
+  output: "standalone",
+  // https://github.com/vercel/next.js/issues/11669
+  compress: false,
 });
